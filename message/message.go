996b5a6615e91eaeec72defb5ae64612494c0a61
hy3 <atelier.hy3@gmail.com>
2015-07-26 15:01:46 +0900

    Add constructers of message structs.

--- message/message.go
+++ message/message.go
@@ -9,10 +9,24 @@ type MessageBox struct {
 	Drawers map[string]*Drawer
 }
 
+// NewMessageBox creates a new MessageBox object with no Drawers.
+func NewMessageBox() *MessageBox {
+	m := new(MessageBox)
+	m.Drawers = make(map[string]*Drawer)
+	return m
+}
+
 // Drawer is a drawer of message box.
 // Every drawer holds messages for someone.
 type Drawer struct {
 	Messages []*Message `json:"messages"`
+}
+
+// NewDrawer creates a new Drawer object with no Messages.
+func NewDrawer() *Drawer {
+	d := new(Drawer)
+	d.Messages = make([]*Message, 0, 10)
+	return d
 }
 
 // Message is a message sent from someone to someone.
@@ -22,4 +36,14 @@ type Message struct {
 	To        string    `json:"to"`
 	Body      string    `json:"body"`
 	Timestamp time.Time `json:"timestamp"`
+}
+
+// New creates a new Message object.
+func New(from, to, body string) *Message {
+	m := new(Message)
+	m.From = from
+	m.To = to
+	m.Body = body
+	m.Timestamp = time.Now()
+	return m
 }
